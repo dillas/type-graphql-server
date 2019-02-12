e907716c9668598372c91d25f695b4609a2eb249
Evgenii Yakobenko <dillas90@gmail.com>
2019-02-12 14:18:15 +0300

    end of lern

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -4,7 +4,7 @@ import { User } from "../../entity/User";
 
 @Resolver(User)
 export class MeResolver {
-  @Query(() => User, { nullable: true })
+  @Query(() => User, { nullable: true, complexity: 7 })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     if (!ctx.req.session!.userId) {
       return undefined;
--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,13 +1,36 @@
 import { buildSchema } from "type-graphql";
+import { ChangePasswordResolver } from "../modules/user/ChangePassword";
+import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
+import {
+  CreaterProductResolver,
+  CreateUserResolver
+} from "../modules/user/CreateUser";
+import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
+import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
+import { MeResolver } from "../modules/user/Me";
+import { ProfilePictureResolver } from "../modules/user/PrifilePicture";
+import { RegisterResolver } from "../modules/user/Register";
 
-export const createSchema = () => buildSchema({
-  resolvers: [__dirname + "/../modules/*/*.ts"],
-  authChecker: ({ context: { req } }) => {
-    // here you can read user from context
-    // and check his permission in db against `roles` argument
-    // that comes from `@Authorized`, eg. ["ADMIN", "MODERATOR"]
+export const createSchema = () =>
+  buildSchema({
+    resolvers: [
+      ChangePasswordResolver,
+      ConfirmUserResolver,
+      ForgotPasswordResolver,
+      LoginResolver,
+      LogoutResolver,
+      MeResolver,
+      RegisterResolver,
+      CreateUserResolver,
+      CreaterProductResolver,
+      ProfilePictureResolver
+    ],
+    authChecker: ({ context: { req } }) => {
+      // here you can read user from context
+      // and check his permission in db against `roles` argument
+      // that comes from `@Authorized`, eg. ["ADMIN", "MODERATOR"]
 
-    return !!req.session.userId;
-  }
-});
-
+      return !!req.session.userId;
+    }
+  });
